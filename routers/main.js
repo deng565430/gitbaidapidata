ab0833f581b63cb5ed6cafbb202b35cd6fbc6759
deng565430 <562340148@qq.com>
2017-04-25 13:32:03 +0800

    new

--- routers/main.js
+++ routers/main.js
@@ -23,6 +23,13 @@ router.get('/list/data', (req, res, next) => {
     var num = req.query.num;
     var database = req.query.database;
     var port = req.query.port;
+    if (table == '' || num == '' || database == '' || port == '') {
+        res.send({
+            msg: '请输入完整数据',
+            code: 0
+        })
+        return;
+    }
     unload(table, num, database, port)
     res.send({
         msg: '正在转换',
@@ -36,11 +43,13 @@ router.get('/list/address', (req, res, next) => {
     var num = req.query.num;
     var database = req.query.database;
     var port = req.query.port;
-
-    console.log(table) 
-    console.log(num)
-    console.log(database)
-    console.log(port)
+    if (table == '' || num == '' || database == '' || port == '') {
+        res.send({
+            msg: '请输入完整数据',
+            code: 0
+        })
+        return;
+    }
     addressXY(table, num, database, port)
     res.send({
         msg: '正在转换',
